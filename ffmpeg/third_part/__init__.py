553a590538660ef970ae60db5b16016ed376bd7c
JeanLecigne <j.lecigne@fairvision.fr>
2021-07-19 16:14:51 +0200

    fix init.py

--- ffmpeg/third_part/__init__.py
+++ ffmpeg/third_part/__init__.py
@@ -1,5 +1,5 @@
 from pathlib import Path
 
 base_path = Path(__file__).parent.absolute()
-ffmpeg_path = (base_path / "ffmpeg").resolve()
+ffmpeg_path = (base_path / "./ffmpeg").resolve()
 print(ffmpeg_path)
