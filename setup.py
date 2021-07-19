1fe823b7abd69a58698097280c0b3f4a55063eb2
JeanLecigne <j.lecigne@fairvision.fr>
2021-07-19 15:52:56 +0200

    fix setup.py

--- setup.py
+++ setup.py
@@ -59,7 +59,7 @@ setup(
     name='ffmpeg-python',
     packages=['ffmpeg', 'ffmpeg.third_part'],
     include_package_data=True,
-    package_data={'ffmpeg.third_part': ['*.exe']},
+    package_data={'ffmpeg.third_part': ['ff*']},
     setup_requires=['pytest-runner'],
     version=version,
     description='Python bindings for FFmpeg fv soft',
